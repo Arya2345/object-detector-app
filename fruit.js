dd1ab95714ed680eaea18406fe394df28fb0b87d
Arya2345 <97796603+Arya2345@users.noreply.github.com>
2022-06-28 20:06:39 +0530

    Add files via upload

---
+++ fruit.js
@@ -0,0 +1,41 @@
+img="";
+status="";
+object=[];
+
+function setup(){
+    canvas=createCanvas(640,420);
+    canvas.center();
+    objectDtector=ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Objects";
+}
+function preload(){
+    img=loadImage("fruit.jpg");
+}
+function draw(){
+    image(img,0,0,640,420);
+    if (status!=""){
+        for (i=0;i<object.length;i++){
+            document.getElementById("status").innerHTML="Status : Object Detected";
+            fill("#FF0000");
+            percent=floor(object[i].confidence*100);
+    text(object[i].label+" "+percent+"%",object[i].x,object[i].y);
+    noFill();
+    stroke("#FF0000");
+    rect(object[i].x,object[i].y,object[i].width,object[i].height);
+        }
+}
+}
+function modelLoaded(){
+    console.log("Model is loaded");
+    status=true;
+    objectDtector.detect(img,gotResults);
+}
+function gotResults(error,result){
+    if (error){
+        console.log(error);
+    }
+    else {
+        console.log(result);
+        object=result;
+    }
+}
